37a37a4ffa3f697da1d2b5955715493a719f72dd
Risto Tõldsep <r.toldsep@gmail.com>
2022-02-23 00:37:20 +0200

    Added different components and got button events working

---
+++ src/components/Button.vue
@@ -0,0 +1,19 @@
+<template>
+    <!--we can use @click instead of v-on:click="" -->
+    <button @click="onClick()" :style="{background: color}" class="btn">{{text}}</button> 
+</template>
+
+<script>
+    export default {
+        name: 'Button',
+        props: {
+            text: String,
+            color: String
+        },
+        methods: {
+            onClick() {
+                console.log("CLICK")
+            }
+        }
+    }
+</script>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,21 @@
+<template>
+    <!-- Vue loops need identical key -->
+    <div :key="task.id" v-for="task in tasks">
+        <Task v-on:delete-task="$emit('delete-task', task.id)" :task="task" />
+    </div>
+</template>
+
+<script>
+    import Task from './Task'
+
+    export default {
+        name: 'Tasks',
+        props: {
+            tasks: Array,
+        },
+        components: {
+            Task,
+        },
+        emits: ['delete-task'] //PASSING IT UP THE FILE ARCHITECTURE TO APP.VUE
+    }
+</script>
